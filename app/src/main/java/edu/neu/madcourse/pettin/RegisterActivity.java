db6dcb5f708886c5c0df950b1b746c52419dee76
Cheng Xue <74628712+moonpiiiiie@users.noreply.github.com>
2022-07-12 14:24:44 -0700

    solve login app crash problem (#5)

--- app/src/main/java/edu/neu/madcourse/pettin/RegisterActivity.java
+++ app/src/main/java/edu/neu/madcourse/pettin/RegisterActivity.java
@@ -1,9 +1,7 @@
 package edu.neu.madcourse.pettin;
 
-import static android.view.View.*;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import static android.view.View.INVISIBLE;
+import static android.view.View.OnClickListener;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,9 +12,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
