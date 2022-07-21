1815c93a19c1539b875a2ebee066e15ba6814b0f
JiarongF <jiaronf1@uci.edu>
2022-07-21 17:27:41 -0400

    add post detail

---
+++ app/src/main/java/edu/neu/madcourse/pettin/Classes/PostAdapter.java
@@ -0,0 +1,151 @@
+package edu.neu.madcourse.pettin.Classes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyViewHolder>  implements Filterable {
+
+    private Context context;
+    private ArrayList<Post> posts;
+    private ArrayList<Post> postsAll;
+    private RecyclerViewClickListener listener;
+    private DatabaseReference database;
+
+    public PostAdapter(Context context, ArrayList<Post> postList, ArrayList<Post> postListAll, RecyclerViewClickListener listener) {
+        this.context = context;
+        this.posts = postList;
+        this.postsAll = postListAll;
+        this.listener = listener;
+        database = FirebaseDatabase.getInstance().getReference("Users");
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Post post = posts.get(position);
+        String username = post.getUsername();
+        String title = post.getTitle();
+        String likes = post.getLikes();
+        holder.title.setText(title.length() <= 20? title: title.substring(0,20) + "...");
+        holder.likes.setText(likes);
+        holder.username.setText(username);
+        Glide.with(context).load(post.getImage()).into(holder.image);
+
+//         Click the username, got some bugs here
+//        holder.username.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(context, ShowUserDetailActivity.class);
+//                intent.putExtra("username", username);
+//                context.startActivity(intent);
+//            }
+//        });
+
+
+        // Click the Likes
+        holder.heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference likesThisPost = database.child(holder.username.getText().toString()).child("posts").child(title).child("likes");
+                likesThisPost.setValue(String.valueOf(Integer.valueOf(likes)+1));
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView title, likes, username;
+        ImageView heart, image;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            likes = itemView.findViewById(R.id.likes);
+            username = itemView.findViewById(R.id.post_username);
+            heart = itemView.findViewById(R.id.heart);
+            image = itemView.findViewById(R.id.image);
+            image.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            listener.onClick(itemView, getAdapterPosition());
+        }
+    }
+
+
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    Filter filter = new Filter() {
+        // background thread
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<Post> filteredList = new ArrayList<>();
+            if(charSequence.toString().isEmpty()) {
+                filteredList.addAll(postsAll);
+            } else {
+                String pattern = charSequence.toString().toLowerCase(Locale.ROOT).trim();
+                // Search for title
+                for(Post post: postsAll) {
+                    if(post.getTitle().toLowerCase().contains(pattern) || (post.getLocation() != null && post.getLocation().toLowerCase().contains(pattern))) {
+                        filteredList.add(post);
+                    }
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+            return filterResults;
+        }
+
+        // UI thread
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            posts.clear();
+            posts.addAll((Collection<? extends Post>) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
+
+
+    public interface RecyclerViewClickListener {
+        void onClick(View v, int position);
+    }
+
+}
+
--- app/src/main/java/edu/neu/madcourse/pettin/PostActivity.java
+++ app/src/main/java/edu/neu/madcourse/pettin/PostActivity.java
@@ -1,23 +1,104 @@
 package edu.neu.madcourse.pettin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import edu.neu.madcourse.pettin.Classes.Post;
+import edu.neu.madcourse.pettin.Classes.PostAdapter;
+import edu.neu.madcourse.pettin.Classes.User;
 
 public class PostActivity extends AppCompatActivity {
     BottomNavigationView bottomNav;
+    private RecyclerView recyclerView;
+    private PostAdapter.RecyclerViewClickListener listener;
     private ImageView addButton;
+    private ImageView homeButton;
+    private ImageView followButton;
+
+    private DatabaseReference database;
+    private PostAdapter postAdapter;
+    private ArrayList<User> userList;
+    private ArrayList<Post> postList;
+    private ArrayList<Post> newPostList;
+
+    private String username;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        recyclerView = findViewById(R.id.postList);
+        database = FirebaseDatabase.getInstance().getReference("Users");
 
-        addButton = findViewById(R.id.add_post_button);
+        addButton = findViewById(R.id.add);
+
+        username = getIntent().getStringExtra("username");
+        PostActivity.this.setTitle(username);
+
+        postList = new ArrayList<>();
+        userList = new ArrayList<>();
+        newPostList = new ArrayList<>();
+
+        setPostItemOnClickListener();
+        postAdapter = new PostAdapter(this, postList, newPostList, listener);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(postAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                postList.clear();
+                newPostList.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    User user = dataSnapshot.getValue(User.class);
+                    if(user.posts != null) {
+                        userList.add(user);
+                    }
+                }
+                getPosts(userList, postList, newPostList);
+                postAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        // add post activity
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PostActivity.this, AddPostActivity.class);
+                intent.putExtra("username", username);
+                startActivity(intent);
+            }
+        });
+
+
         bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setSelectedItemId(R.id.nav_post);
         bottomNav.setOnItemSelectedListener(item -> {
@@ -50,4 +131,83 @@ public class PostActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * The click listener for each item in the recycler list
+     */
+    private void setPostItemOnClickListener() {
+        listener = new PostAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Toast.makeText(PostActivity.this, "Title is:" + postList.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+                Intent toPostDetailPage = new Intent(getApplicationContext(), ShowPostDetailActivity.class);
+                toPostDetailPage.putExtra("content", postList.get(position).getContent());
+                toPostDetailPage.putExtra("image", postList.get(position).getImage());
+                toPostDetailPage.putExtra("likes", postList.get(position).getLikes());
+                toPostDetailPage.putExtra("location", postList.get(position).getLocation());
+                toPostDetailPage.putExtra("time", postList.get(position).getTime());
+                toPostDetailPage.putExtra("title", postList.get(position).getTitle());
+                toPostDetailPage.putExtra("bloggerName", postList.get(position).getUsername());
+                toPostDetailPage.putExtra("username", username);
+                startActivity(toPostDetailPage);
+            }
+        };
+    }
+
+
+    /**
+     * Get all the posts from the user and update the post list
+     * @param userList
+     * @param postList
+     */
+    private void getPosts(ArrayList<User> userList, ArrayList<Post> postList, ArrayList<Post> newPostList) {
+        for(User user: userList) {
+            String username = user.getUsername();
+            for(Map.Entry<String, Object> entry: user.getPosts().entrySet()) {
+                Map<String, String> map = (Map<String, String>)entry.getValue();
+                String image = map.get("image");
+                String title = map.get("title");
+                String content = map.get("content");
+                String location = map.get("location");
+                String time = map.get("time");
+                String likes = map.get("likes");
+                postList.add(new Post(image, title, content, location, time, likes, username));
+                newPostList.add(new Post(image, title, content, location, time, likes, username));
+            }
+        }
+        postList.sort((post1, post2) -> Integer.valueOf(post2.getLikes()) - Integer.valueOf(post1.getLikes()));
+        newPostList.sort((post1, post2) -> Integer.valueOf(post2.getLikes()) - Integer.valueOf(post1.getLikes()));
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
+//        MenuItem item = menu.findItem(R.id.search);
+//        SearchView searchView = (SearchView) item.getActionView();
+//        searchView.setQueryHint("Search Title or Location");
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                postAdapter.getFilter().filter(query);
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                return false;
+//            }
+//        });
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if(item.getItemId() == R.id.profile) {
+//            Intent toProfilePage = new Intent(PostActivity.this, ShowUserDetailActivity.class);
+//            toProfilePage.putExtra("username", username);
+//            startActivity(toProfilePage);
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
