cd80d7349e0ed4d0bcb6fb86a47357cd78c8c9e8
Cheng Xue <74628712+moonpiiiiie@users.noreply.github.com>
2022-07-31 22:54:51 -0700

    show username on profile activity (#16)

--- app/src/main/java/edu/neu/madcourse/pettin/ProfileActivity.java
+++ app/src/main/java/edu/neu/madcourse/pettin/ProfileActivity.java
@@ -6,20 +6,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import edu.neu.madcourse.pettin.Classes.User;
 
 public class ProfileActivity extends AppCompatActivity {
     BottomNavigationView bottomNav;
     Button button_SignOut;
-
+    FirebaseFirestore db;
     FirebaseAuth auth;
+    FirebaseUser curUser;
+    String userName;
+    String email;
+    TextView textView_userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         auth = FirebaseAuth.getInstance();
+        curUser = auth.getCurrentUser();
+
+        textView_userName = findViewById(R.id.textView_userName);
+        if (curUser != null) {
+            String userId = curUser.getUid();
+            db = FirebaseFirestore.getInstance();
+            DocumentReference userRef = db.collection("users").document(userId);
+            userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    User user = documentSnapshot.toObject(User.class);
+                    userName = user.getUsername();
+                    email = user.getEmail();
+                    textView_userName.setText(userName);
+                }
+            });
+        }
         button_SignOut = findViewById(R.id.button_Signout);
         button_SignOut.setOnClickListener(new View.OnClickListener() {
             @Override
