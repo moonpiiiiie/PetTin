fc3024516e09c5f25469a3184cf99bb4cdec7189
Cheng Xue <74628712+moonpiiiiie@users.noreply.github.com>
2022-08-08 23:13:58 -0700

    access firestore (#47)

--- app/src/main/java/edu/neu/madcourse/pettin/SingleDogActivity.java
+++ app/src/main/java/edu/neu/madcourse/pettin/SingleDogActivity.java
@@ -1,28 +1,24 @@
 package edu.neu.madcourse.pettin;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
@@ -33,13 +29,13 @@ import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import edu.neu.madcourse.pettin.Classes.Dogs;
 import edu.neu.madcourse.pettin.Classes.User;
 
 public class SingleDogActivity extends AppCompatActivity {
+    final String TAG = "single dog activity";
     //Post section
     Dogs curDog;
     TextView dogName, dogCity, dogAge, dogGender, dogBreed;
@@ -58,9 +54,14 @@ public class SingleDogActivity extends AppCompatActivity {
     ExtendedFloatingActionButton dislike, match;
 
     // match my dog dialog
-    List<String> myDogtoMatch;
+    List<String> myDog;
+    List<Task<DocumentSnapshot>> task;
+    List<String> myDogToMatch;
     DogToMatchAdapter dogToMatchAdapter;
     RecyclerView myDogToMatchRV;
+    int dogNum;
+    DocumentReference curDogRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,7 +74,6 @@ public class SingleDogActivity extends AppCompatActivity {
         dogAge = findViewById(R.id.textView_age);
         dogGender = findViewById(R.id.textView_gender);
         dogBreed = findViewById(R.id.textView_dogBreed);
-
 
 
         // data carried from main activity
@@ -92,7 +92,7 @@ public class SingleDogActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
 
         // retrieve data from firebase for the single post
-        DocumentReference curDogRef = db.collection("dogs").document(dog_id);
+        curDogRef = db.collection("dogs").document(dog_id);
         curDogRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
@@ -112,7 +112,15 @@ public class SingleDogActivity extends AppCompatActivity {
         curUser = firebaseAuth.getCurrentUser();
         // buttons
         dislike = findViewById(R.id.button_dislike);
-        match=findViewById(R.id.button_match);
+        match = findViewById(R.id.button_match);
+
+
+        // get dog number
+        dogNum = 0;
+        myDog = new ArrayList<>();
+        myDogToMatch = new ArrayList<>();
+        dogNum = ownedDogs();
+        System.out.println("dog " + dogNum);
 
         dislike.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -134,8 +142,18 @@ public class SingleDogActivity extends AppCompatActivity {
         match.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getMyDogList();
-                showMatchDialog();
+                // part I
+                if (dogNum == 0) {
+
+                    Toast.makeText(SingleDogActivity.this, "Please add your dog to match another dog", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (dogNum > 1) {
+                    System.out.println("dogNum>1");
+                    System.out.println("my dog to match" + myDogToMatch);
+                    showMatchDialog();
+                }
+
 //                AlertDialog.Builder builderSingle = new AlertDialog.Builder(SingleDogActivity.this);
 //
 //                builderSingle.setTitle("Select One Pet:-");
@@ -171,100 +189,148 @@ public class SingleDogActivity extends AppCompatActivity {
 //                    }
 //                });
 //                builderSingle.show();
+                // part II
 
-//                if (curUser != null) {
-//                    String userId = curUser.getUid();
-//                    DocumentReference userRef = db.collection("users").document(curUser.getUid());
-//                    userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//                        @Override
-//                        public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                            User user = documentSnapshot.toObject(User.class);
-//                            List<String> dislikeDogs = user.getDislikeDog();
-//
-//                            // TODO pop up a dialog to choose user's dog to match
-//                            System.out.println("user dog number " + user.getDogs().size());
-////                            if (user.getDogs().size()>=2) {
-////
-////                            }
-//                            String dogId = user.getDogs().get(0);
-//                            if (dislikeDogs.contains(curDog.getDog_id())) {
-//                                userRef.update("dislikeDog", FieldValue.arrayRemove(curDog.getDog_id()));
-//                            }
-////                            // TODO add to sent match list with current dog
-//                            DocumentReference myDogRef = db.collection("dogs").document(dogId);
-//                            myDogRef.update("sentMatch", FieldValue.arrayUnion(curDog.getDog_id()));
-////                            // TODO add received match with the other dog
-//                            curDogRef.update("receivedMatch", FieldValue.arrayUnion(curDog.getDog_id()));
-////                            // TODO check if sent and received
-//
-//                        }
-//                    });
-//                    finish();
-//
-//                } else {
-//                    Toast.makeText(SingleDogActivity.this, "Please log in to match a playdate", Toast.LENGTH_SHORT).show();
-//                }
             }
         });
 
     }
+
     void updateDislikeDogList(String dogId) {
 
     }
 
     void getMyDogList() {
-        myDogtoMatch = new ArrayList<>();
+
+    }
+
+    // get how many dogs the current user owned
+    int ownedDogs() {
         DocumentReference userRef = db.collection("users").document(curUser.getUid());
         CollectionReference dogRef = db.collection("dogs");
         userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                 User user = documentSnapshot.toObject(User.class);
-                for (String dogId: user.getDogs()) {
-                    myDogtoMatch.add(dogId);
-                }
-                System.out.println("my dog to match" + myDogtoMatch);
+
+                myDog = user.getDogs();
+                dogNum = myDog.size();
             }
         });
+        List<Task<DocumentSnapshot>> tasks = new ArrayList<>();
+        System.out.println("my dog" + myDog);
+        for (String path : myDog) {
+            tasks.add(db.document(path).get());
+        }
+        System.out.println("tasks " + tasks);
+        Task<List<DocumentSnapshot>> finalTask = Tasks.whenAllSuccess(tasks);
+        finalTask.addOnSuccessListener(new OnSuccessListener<List<DocumentSnapshot>>() {
+            @Override
+            public void onSuccess(List<DocumentSnapshot> documentSnapshots) {
+                System.out.println("onSuccess?");
+                for (DocumentSnapshot snapshot : documentSnapshots) {
+                    System.out.println(snapshot.toObject(Dogs.class).getName());
+                    myDogToMatch.add(snapshot.toObject(Dogs.class).getName());
+                }
+
+            }
+        });
+//        Task<List<DocumentSnapshot>> task = Tasks.whenAllComplete(myDog);
+
+//        System.out.println("my dog " + myDog);
+//        for (String dogId: myDog) {
+//           dogRef.document(dogId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                @Override
+//                public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                    System.out.println("get name " + documentSnapshot.toObject(Dogs.class).getName());
+//                    myDogToMatch.add(documentSnapshot.toObject(Dogs.class).getName());
+//                }
+//            });
+//        }
+        System.out.println("my dog to match" + myDogToMatch);
+        return dogNum;
     }
 
     void showMatchDialog() {
-            AlertDialog.Builder builderSingle = new AlertDialog.Builder(SingleDogActivity.this);
+        AlertDialog.Builder builderSingle = new AlertDialog.Builder(SingleDogActivity.this);
 
-            builderSingle.setTitle("Select One Pet:-");
+        builderSingle.setTitle("Select One Pet:-");
 
-            final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(SingleDogActivity.this, android.R.layout.select_dialog_singlechoice);
-            arrayAdapter.add("Hardik");
-            arrayAdapter.add("Archit");
-            arrayAdapter.add("Jignesh");
-            arrayAdapter.add("Umang");
-            arrayAdapter.add("Gatti");
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(SingleDogActivity.this, android.R.layout.select_dialog_singlechoice);
 
-            builderSingle.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
+        arrayAdapter.add("Hardik");
+        arrayAdapter.add("Archit");
+        arrayAdapter.add("Jignesh");
+        arrayAdapter.add("Umang");
+        arrayAdapter.add("Gatti");
 
-            builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    String strName = arrayAdapter.getItem(which);
-                    AlertDialog.Builder builderInner = new AlertDialog.Builder(SingleDogActivity.this);
-                    builderInner.setMessage(strName);
-                    builderInner.setTitle("Your Selected Pet is");
-                    builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog,int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    builderInner.show();
-                }
-            });
-            builderSingle.show();
+        builderSingle.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String strName = arrayAdapter.getItem(which);
+                AlertDialog.Builder builderInner = new AlertDialog.Builder(SingleDogActivity.this);
+                builderInner.setMessage(strName);
+                builderInner.setTitle("Your Selected Pet is");
+                builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builderInner.show();
+            }
+        });
+        builderSingle.show();
 
 
     }
+
+    void fireStoreMatch() {
+        if (curUser != null) {
+            String userId = curUser.getUid();
+            DocumentReference userRef = db.collection("users").document(curUser.getUid());
+            userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    User user = documentSnapshot.toObject(User.class);
+                    List<String> dislikeDogs = user.getDislikeDog();
+
+                    // TODO pop up a dialog to choose user's dog to match
+                    System.out.println("user dog number " + user.getDogs().size());
+//                            if (user.getDogs().size()>=2) {
+//
+//                            }
+                    String dogId = user.getDogs().get(0);
+                    if (dislikeDogs.contains(curDog.getDog_id())) {
+                        userRef.update("dislikeDog", FieldValue.arrayRemove(curDog.getDog_id()));
+                    }
+//
+                    DocumentReference myDogRef = db.collection("dogs").document(dogId);
+                    myDogRef.update("sentMatch", FieldValue.arrayUnion(curDog.getDog_id()));
+//
+                    curDogRef.update("receivedMatch", FieldValue.arrayUnion(curDog.getDog_id()));
+//                            // TODO check if sent and received
+
+                }
+            });
+            finish();
+
+        } else {
+            Toast.makeText(SingleDogActivity.this, "Please log in to match a playdate", Toast.LENGTH_SHORT).show();
+        }
+    }
+//    class fireStoreMatch implements Runnable {
+//
+//        @Override
+//        public void run() {
+//
+//        }
+//    }
 }
