d3fdd8578c4aaefcf430ac89e9730f882d24c829
JiarongF <72358925+JiarongF@users.noreply.github.com>
2022-08-11 16:28:04 -0400

    Jf (#59)
    * add post(not done)
    
    * add post detail
    
    * add post detail
    
    * add follow activity
    
    * fetch from main
    
    * use chop image to upload post and add comment of posts
    
    * have problem with fragment
    
    * remove fragment
    
    * fix show location

---
+++ app/src/main/java/edu/neu/madcourse/pettin/ShowPostDetailActivity.java
@@ -0,0 +1,80 @@
+package edu.neu.madcourse.pettin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+
+import edu.neu.madcourse.pettin.Classes.Post;
+
+public class ShowPostDetailActivity extends AppCompatActivity {
+    Post curPost;
+    private String image;
+
+    private String post_id;
+
+    // firebase
+    FirebaseUser firebaseUser;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore db;
+
+    private ImageView postDetailImg;
+    private TextView postDetailUsername;
+    private TextView postDetailTitle;
+    private TextView postDetailContent;
+//    private TextView postDetailLikes;
+    private TextView postDetailLocation;
+    private TextView postDetailTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_post_detail);
+        post_id = getIntent().getStringExtra("postId");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+
+        postDetailImg = findViewById(R.id.postDetailImg);
+        postDetailUsername = findViewById(R.id.postDetailUsername);
+        postDetailTitle = findViewById(R.id.postDetailTitle);
+        postDetailContent = findViewById(R.id.postDetailContent);
+//        postDetailLikes = findViewById(R.id.postDetailLikes);
+        postDetailLocation = findViewById(R.id.postDetailLocation);
+        postDetailTime = findViewById(R.id.postDetailTime);
+
+
+
+        DocumentReference postRef = db.collection("posts").document(post_id);
+        postRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                curPost = documentSnapshot.toObject(Post.class);
+
+                postDetailUsername.setText(curPost.getUsername());
+                postDetailTitle.setText(curPost.getTitle());
+                postDetailContent.setText(curPost.getContent());
+//                postDetailLikes.setText(curPost.getLikes());
+                postDetailLocation.setText(curPost.getLocation());
+                postDetailTime.setText(curPost.getTime());
+                image = curPost.getImage();
+                Picasso.with(ShowPostDetailActivity.this).load(image).into(postDetailImg);
+            }
+        });
+
+
+    }
+}
