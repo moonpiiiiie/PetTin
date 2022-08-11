0e87435a1b97e762e57b0ccace81b16af25564fc
Cheng Xue <cheng.xue910@gmail.com>
2022-08-11 10:46:49 -0700

    update profileActivity layout

--- app/src/main/java/edu/neu/madcourse/pettin/ChatActivity.java
+++ app/src/main/java/edu/neu/madcourse/pettin/ChatActivity.java
@@ -94,37 +94,37 @@ public class ChatActivity extends AppCompatActivity implements UserListenerInter
 //    /**
 //     * Method sets up the tab layout.
 //     */
-//    private void setTabLayout() {
-//        tabLayout = findViewById(R.id.chats_tab_layout);
-//        viewPager2 = findViewById(R.id.view_pager);
-//        viewPageAdapter = new ViewPageAdapter(this);
-//        viewPager2.setAdapter(viewPageAdapter);
-//
-//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                viewPager2.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
-//
-//        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-//            @Override
-//            public void onPageSelected(int position) {
-//                super.onPageSelected(position);
-//                tabLayout.getTabAt(position).select();
-//            }
-//        });
-//    }
+    private void setTabLayout() {
+        tabLayout = findViewById(R.id.chats_tab_layout);
+        viewPager2 = findViewById(R.id.view_pager);
+        viewPageAdapter = new ViewPageAdapter(this);
+        viewPager2.setAdapter(viewPageAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+    }
 
     /**
 
