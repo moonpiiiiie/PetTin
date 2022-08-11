fccb03fe284af52bb0ff6558bafe19a0e245a20b
tifflastimosa <99374099+tifflastimosa@users.noreply.github.com>
2022-08-10 18:48:32 -0700

    Group Member Chat
    add logic to add group members to chat.

--- app/src/main/java/edu/neu/madcourse/pettin/GroupChat/CreateGroupChatActivity.java
+++ app/src/main/java/edu/neu/madcourse/pettin/GroupChat/CreateGroupChatActivity.java
@@ -2,14 +2,32 @@ package edu.neu.madcourse.pettin.GroupChat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.util.ArrayList;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import edu.neu.madcourse.pettin.ChatActivity;
+import edu.neu.madcourse.pettin.Classes.User;
+import edu.neu.madcourse.pettin.GroupChat.UserMatches.MatchedUsersAdapter;
 import edu.neu.madcourse.pettin.R;
 
 
@@ -18,9 +36,18 @@ public class CreateGroupChatActivity extends AppCompatActivity {
     private static final String TAG = "CreateGroupChatActivity ";
 
     private EditText groupName;
+    private ImageView backButton;
 
     // get the current year
     private FirebaseUser currentUser;
+
+    // android widgets
+    private RecyclerView matchedUsersRV;
+    private MatchedUsersAdapter matchedUsersAdapter;
+
+    private ArrayList<User> listOfUsers;
+
+
 
     // get db instance
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -32,12 +59,60 @@ public class CreateGroupChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group_chat);
 
+        backButton();
+
+        // recycler view to display matched users to add to group
+        matchedUsersRV = findViewById(R.id.create_group_matched_users);
+
         // need to build the group chat with title, id, participants
+        retrieveUsers();
 
 
     }
 
+    private void backButton() {
+        backButton = findViewById(R.id.create_group_back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreateGroupChatActivity.this, ChatActivity.class));
+            }
+        });
+    }
+
+    private void retrieveUsers() {
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        matchedUsersRV = findViewById(R.id.create_group_matched_users);
+        matchedUsersRV.setHasFixedSize(true);
+        matchedUsersRV.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        db = FirebaseFirestore.getInstance();
+        listOfUsers = new ArrayList<User>();
+        matchedUsersAdapter = new MatchedUsersAdapter(CreateGroupChatActivity.this, this.listOfUsers);
 
 
+        DocumentReference currentUserDocRef = db.collection("users").document(currentUser.getUid());
+        currentUserDocRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (value != null && value.exists()) {
+                    User user = value.toObject(User.class);
+                    for (User matchedUser : user.getMatchedUsers()) {
+                        listOfUsers.add(matchedUser);
+                    }
+                    matchedUsersAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        matchedUsersRV.setAdapter(matchedUsersAdapter);
+        ArrayList<User> groupMembers = (ArrayList<User>) matchedUsersAdapter
+                                            .getGroupMembers()
+                                            .stream()
+                                            .distinct()
+                                            .collect(Collectors.toList());
+        Log.v(TAG + "size of groupMembers", String.valueOf(groupMembers.size()));
+
+    }
 
 }
--- app/src/main/java/edu/neu/madcourse/pettin/GroupChat/UserMatches/MatchedUsersAdapter.java
+++ app/src/main/java/edu/neu/madcourse/pettin/GroupChat/UserMatches/MatchedUsersAdapter.java
@@ -1,6 +1,5 @@
 package edu.neu.madcourse.pettin.GroupChat.UserMatches;
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -9,11 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -21,33 +17,32 @@ import edu.neu.madcourse.pettin.Classes.User;
 import edu.neu.madcourse.pettin.GroupChat.Messages.MessageActivity;
 import edu.neu.madcourse.pettin.R;
 
+public class MatchedUsersAdapter extends RecyclerView.Adapter<MatchedUsersAdapter.MatchedUserViewHolder> {
 
-public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
-
-    private static final String TAG = "UserAdapter";
+    private static final String TAG = "MatchedUserAdapter ";
 
     private Context context;
     private ArrayList<User> listOfUsers;
+    private ArrayList<User> groupMembers;
     private User user;
 
-    public UserAdapter(Context context, ArrayList<User> listOfUsers) {
+    public MatchedUsersAdapter(Context context, ArrayList<User> listOfUsers) {
         this.context = context;
         this.listOfUsers = listOfUsers;
+        this.groupMembers = new ArrayList<>();
     }
 
     @NonNull
     @Override
-    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.user_card_chat, parent, false);
-        return new UserViewHolder(view);
+    public MatchedUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MatchedUserViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_create_group_user_card, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MatchedUserViewHolder holder, int position) {
         user = listOfUsers.get(position);
         holder.username.setText(user.getUsername());
         holder.userId = user.getUserId();
-
     }
 
     @Override
@@ -55,33 +50,33 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         return listOfUsers.size();
     }
 
-    public class UserViewHolder extends RecyclerView.ViewHolder {
+    public class MatchedUserViewHolder extends RecyclerView.ViewHolder {
 
         private TextView username;
         private String userId;
 
-        public UserViewHolder(@NonNull View itemView) {
+        public MatchedUserViewHolder(@NonNull View itemView) {
             super(itemView);
-            username = itemView.findViewById(R.id.username);
 
-
-       
-
+            username = itemView.findViewById(R.id.username_current);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Log.v(TAG + " user clicked", username.getText().toString());
                     Log.v(TAG + " username onClick: ", userId);
-                    Intent intent = new Intent(context, MessageActivity.class);
                     // need id - need to pass the id of the user
-                    intent.putExtra("userId", userId);
                     Log.v(TAG, "Starting Message Activity " + userId);
-                    context.startActivity(intent);
+                    groupMembers.add(user);
+                    Log.v(TAG, String.valueOf(groupMembers.contains(user)));
+                    Log.v(TAG + "groupMembers size", String.valueOf(groupMembers.size()));
                 }
             });
-
-
         }
     }
+
+    public ArrayList<User> getGroupMembers() {
+        return groupMembers;
+    }
+
 }
